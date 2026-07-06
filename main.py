4b4c9360c24c4fe0e27e800bdfdf8aab26f37fcf
yriegjibnl-design <yriegjibnl@gmail.com>
2026-07-06 10:01:35 +0330

    Update BOT_TOKEN in main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ from database import init_db, is_user_admin, get_or_create_user, update_stats, g
 # ==========================================
 # تنظیمات اصلی ربات از طریق محیط سرور
 # ==========================================
-BOT_TOKEN = os.getenv("BOT_TOKEN", "8894117383:AAFGeDmC1lnY_LoFaah7zTAX7NjriIb2-Tc")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8894117383:AAFqv00G_eAFkeP0x-UhrENKByEb5U5_MnM")
 INITIAL_ADMIN_ID = int(os.getenv("ADMIN_ID", "7430881772"))
 
 logging.basicConfig(
